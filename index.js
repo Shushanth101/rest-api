ba3ec5e38b34d899425402b9e3548b26d362db76
shushanth <itsmeshus07@gmail.com>
2024-12-07 15:16:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express=  require("express");
+const users=require("./MOCK_DATA.json")
+const fs=require("fs");
+const { error } = require("console");
+
+const app=express();
+const PORT=8080;
+//Middleware
+app.use(express.urlencoded({extended : false}))
+//routes
+app.get("/users",(req,res)=>{
+    const html=`
+    <ul>
+        ${users.map(user=>
+            `<li>${user.first_name}</li>`
+        ).join('')}
+    </ul>
+    `;
+    return res.send(html);
+});
+//REST-API
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+app.get("/api/users/:id",(req,res)=>{
+    let id=Number(req.params.id);
+    const user=users.find((user)=> user.id === id);
+    return res.json(user);
+});
+
+app.post("/api/users",(req,res)=>{
+    //TODO:
+    const body = req.body;
+users.push({ ...body, id: users.length + 1 });
+
+fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+    if (err) {
+        return res.status(500).send("Problem on our side, please try later.");
+    } else {
+        return res.status(201).json({
+            status: "Success",
+            id: users.length
+        });
+    }
+});
+
+console.log(req.body);
+
+  
+});
+
+app.patch("/api/users/:id", (req, res) => {
+    const body = req.body;
+    let id = parseInt(req.params.id, 10);  // Convert id to an integer
+
+    if (id < 1 || id > users.length) {
+        return res.status(400).json({ status: "Invalid user ID" });
+    }
+
+    // Update the user's details
+    users[id - 1].first_name = body.first_name;
+    users[id - 1].last_name = body.last_name;
+    users[id - 1].email = body.email;
+    users[id - 1].gender = body.gender;
+
+    // Write updated data to file
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).send("Problem on our side.");
+        } else {
+            return res.json({ status: "successful" });
+        }
+    });
+});
+
+app.delete("/api/users/:id",(req,res)=>{
+    const body=req.body;
+    users.pop({...body,id: req.params.id});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>{
+        if(err){
+            res.send("problem on our side.");
+        }
+        else{
+            return res.json({ status: "successfull"})
+        }
+    })
+})
+
+app.listen(PORT,()=>console.log(`Server Started! at ${PORT}`));
